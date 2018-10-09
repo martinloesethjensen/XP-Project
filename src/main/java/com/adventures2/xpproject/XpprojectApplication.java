164c549a0c627334df4f8d60a37a8a06d289a3c9
Valhalla <martinfloorball1997@gmail.com>
2018-10-09 09:33:46 +0200

    Initial commit

---
+++ src/main/java/com/adventures2/xpproject/XpprojectApplication.java
@@ -0,0 +1,12 @@
+package com.adventures2.xpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpprojectApplication.class, args);
+	}
+}
