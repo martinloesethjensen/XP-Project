a0dc739858783bc53a4e623ef5387a66b5c8092b
Valhalla <martinfloorball1997@gmail.com>
2018-10-09 11:26:24 +0200

    added gui for login page

--- src/main/java/com/adventures2/xpproject/Controller/SessionController.java
+++ src/main/java/com/adventures2/xpproject/Controller/SessionController.java
@@ -3,6 +3,7 @@ package com.adventures2.xpproject.Controller;
 import com.adventures2.xpproject.DatabaseConnection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SessionController {
@@ -12,4 +13,23 @@ public class SessionController {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         return "/index";
     }
+
+    @GetMapping("/homepage")
+    public String viewHomepage() {
+
+        return "/homepage";
+    }
+    @GetMapping("/login")
+    public String viewLogin() {
+
+        return "/login";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+
+        return "/login";
+    }
+
+
 }
