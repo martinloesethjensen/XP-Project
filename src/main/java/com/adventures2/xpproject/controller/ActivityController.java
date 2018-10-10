40136c7b6ae7deaef58f7374cf232495b3a3c090
ComputerScienceSean <seangunnar96@gmail.com>
2018-10-10 12:03:21 +0200

    Added new reservation button in index

--- src/main/java/com/adventures2/xpproject/controller/ActivityController.java
+++ src/main/java/com/adventures2/xpproject/controller/ActivityController.java
@@ -1,4 +1,13 @@
 package com.adventures2.xpproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ActivityController {
+
+    @GetMapping("/activity_list")
+    public String viewActivities(){
+        return ("/activity_list");
+    }
 }
