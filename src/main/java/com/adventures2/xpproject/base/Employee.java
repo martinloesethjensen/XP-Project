f77be709f8b229d3b0d4e55f182d46d27e27fd80
Martinloesethjensen <martinloesethjensen@gmail.com>
2018-10-13 11:38:34 +0200

    - Added a HandleDataFromDB that right now handles data being collected from the database and being put in their respective lists (Arraylist and HashMaps). - Fixed field in activities that took in an int but it needed to take in a String. - Now the populate method in ReservationController doesn't handle dummy data for lists being placed in the html. - The index page now collects the right data from the database instead of dummy data. - Some base classes have had a small fix on getters, setters, field and toString().

--- src/main/java/com/adventures2/xpproject/base/Employee.java
+++ src/main/java/com/adventures2/xpproject/base/Employee.java
@@ -1,35 +1,42 @@
 package com.adventures2.xpproject.base;
 
 public class Employee {
-    private int id;
-    private String realname;
+	private int id;
+	private String realname;
 
-    public Employee() {
-    }
+	public Employee() {
+	}
 
-    public Employee(int id, String realname) {
-        this.id = id;
-        this.realname = realname;
-    }
+	public Employee(int id, String realname) {
+		this.id = id;
+		this.realname = realname;
+	}
 
-    public Employee(int id) {
-        this.id = id;
-    }
+	public Employee(int id) {
+		this.id = id;
+	}
 
+	@Override
+	public String toString() {
+		return "Employee{" +
+			"id=" + id +
+			", realname='" + realname + '\'' +
+			'}';
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getRealname() {
-        return realname;
-    }
+	public String getRealname() {
+		return realname;
+	}
 
-    public void setRealname(String realname) {
-        this.realname = realname;
-    }
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
 }
--- src/main/java/com/adventures2/xpproject/base/Reservation.java
+++ src/main/java/com/adventures2/xpproject/base/Reservation.java
@@ -1,111 +1,127 @@
 package com.adventures2.xpproject.base;
 
-public class Reservation  {
-  private int id;
-  private String start;
-  private String end;
-  private int customDiscount;
-  private int peopleAmount;
-  private int fk_customer_id;
-  private int fk_activity_id;
-  private int fk_user_id;
+public class Reservation {
+	private int id;
+	private String start;
+	private String end;
+	private int customDiscount;
+	private int peopleAmount;
+	private int fk_customer_id;
+	private int fk_activity_id;
+	private int fk_user_id;
+	private int fk_employee_id;
 
-  public Reservation() {
-  }
+	public Reservation() {
+	}
 
-  public Reservation(String start, String end, int customDiscount, int peopleAmount, int fk_customer_id, int fk_activity_id, int fk_user_id) {
-    this.start = start;
-    this.end = end;
-    this.customDiscount = customDiscount;
-    this.peopleAmount = peopleAmount;
-    this.fk_customer_id = fk_customer_id;
-    this.fk_activity_id = fk_activity_id;
-    this.fk_user_id = fk_user_id;
-  }
+	public Reservation(String start, String end, int customDiscount, int peopleAmount, int fk_customer_id, int fk_activity_id, int fk_user_id, int fk_employee_id) {
+		this.start = start;
+		this.end = end;
+		this.customDiscount = customDiscount;
+		this.peopleAmount = peopleAmount;
+		this.fk_customer_id = fk_customer_id;
+		this.fk_activity_id = fk_activity_id;
+		this.fk_user_id = fk_user_id;
+		this.fk_employee_id = fk_employee_id;
+	}
 
-  public Reservation(int id, String start, String end, int customDiscount, int peopleAmount, int fk_customer_id, int fk_activity_id, int fk_user_id) {
-    this.id = id;
-    this.start = start;
-    this.end = end;
-    this.customDiscount = customDiscount;
-    this.peopleAmount = peopleAmount;
-    this.fk_customer_id = fk_customer_id;
-    this.fk_activity_id = fk_activity_id;
-    this.fk_user_id = fk_user_id;
-  }
+	public Reservation(int id, String start, String end, int customDiscount, int peopleAmount, int fk_customer_id, int fk_activity_id, int fk_user_id, int fk_employee_id) {
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		this.customDiscount = customDiscount;
+		this.peopleAmount = peopleAmount;
+		this.fk_customer_id = fk_customer_id;
+		this.fk_activity_id = fk_activity_id;
+		this.fk_user_id = fk_user_id;
+		this.fk_employee_id = fk_employee_id;
+	}
 
-  @Override
-  public String toString() {
-    return "Reservation{" +
-            "id=" + id +
-            ", start='" + start + '\'' +
-            ", end='" + end + '\'' +
-            ", customDiscount=" + customDiscount +
-            ", peopleAmount=" + peopleAmount +
-            ", fk_customer_id=" + fk_customer_id +
-            ", fk_activity_id=" + fk_activity_id +
-            ", fk_user_id=" + fk_user_id +
-            '}';
-  }
+	@Override
+	public String toString() {
+		return "Reservation{" +
+			"id=" + id +
+			", start='" + start + '\'' +
+			", end='" + end + '\'' +
+			", customDiscount=" + customDiscount +
+			", peopleAmount=" + peopleAmount +
+			", fk_customer_id=" + fk_customer_id +
+			", fk_activity_id=" + fk_activity_id +
+			", fk_user_id=" + fk_user_id +
+			", fk_employee_id=" + fk_employee_id +
+			'}';
+	}
 
-  public int getId() {
-    return id;
-  }
+	public int getId() {
+		return id;
+	}
 
-  public String getStart() {
-    return start;
-  }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-  public void setStart(String start) {
-    this.start = start;
-  }
+	public String getStart() {
+		return start;
+	}
 
-  public String getEnd() {
-    return end;
-  }
+	public void setStart(String start) {
+		this.start = start;
+	}
 
-  public void setEnd(String end) {
-    this.end = end;
-  }
+	public String getEnd() {
+		return end;
+	}
 
-  public int getCustomDiscount() {
-    return customDiscount;
-  }
+	public void setEnd(String end) {
+		this.end = end;
+	}
 
-  public void setCustomDiscount(int customDiscount) {
-    this.customDiscount = customDiscount;
-  }
+	public int getCustomDiscount() {
+		return customDiscount;
+	}
 
-  public int getPeopleAmount() {
-    return peopleAmount;
-  }
+	public void setCustomDiscount(int customDiscount) {
+		this.customDiscount = customDiscount;
+	}
 
-  public void setPeopleAmount(int peopleAmount) {
-    this.peopleAmount = peopleAmount;
-  }
+	public int getPeopleAmount() {
+		return peopleAmount;
+	}
 
-  public int getFk_customer_id() {
-    return fk_customer_id;
-  }
+	public void setPeopleAmount(int peopleAmount) {
+		this.peopleAmount = peopleAmount;
+	}
 
-  public void setFk_customer_id(int fk_customer_id) {
-    this.fk_customer_id = fk_customer_id;
-  }
+	public int getFk_customer_id() {
+		return fk_customer_id;
+	}
 
-  public int getFk_activity_id() {
-    return fk_activity_id;
-  }
+	public void setFk_customer_id(int fk_customer_id) {
+		this.fk_customer_id = fk_customer_id;
+	}
 
-  public void setFk_activity_id(int fk_activity_id) {
-    this.fk_activity_id = fk_activity_id;
-  }
+	public int getFk_activity_id() {
+		return fk_activity_id;
+	}
 
-  public int getFk_user_id() {
-    return fk_user_id;
-  }
+	public void setFk_activity_id(int fk_activity_id) {
+		this.fk_activity_id = fk_activity_id;
+	}
 
-  public void setFk_user_id(int fk_user_id) {
-    this.fk_user_id = fk_user_id;
-  }
+	public int getFk_user_id() {
+		return fk_user_id;
+	}
+
+	public void setFk_user_id(int fk_user_id) {
+		this.fk_user_id = fk_user_id;
+	}
+
+	public int getFk_employee_id() {
+		return fk_employee_id;
+	}
+
+	public void setFk_employee_id(int fk_employee_id) {
+		this.fk_employee_id = fk_employee_id;
+	}
 }
 
