c3e5cf95683976d2631532c40553d4989a64d32e
Martinloesethjensen <martinloesethjensen@gmail.com>
2018-10-15 14:56:53 +0200

    Applied thoughts on how to make the search function.

--- src/main/java/com/adventures2/xpproject/controller/ReservationController.java
+++ src/main/java/com/adventures2/xpproject/controller/ReservationController.java
@@ -16,60 +16,70 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class ReservationController {
-	private String successMessage = "";
-	HandleDataFromDB handleDataFromDB = new HandleDataFromDB();
+    private String successMessage = "";
+    HandleDataFromDB handleDataFromDB = new HandleDataFromDB();
 
 
-	public void populate() {
+    public void populate() {
 
-		handleDataFromDB.clearLists();
+        handleDataFromDB.clearLists();
 
-		handleDataFromDB.populateLists();
+        handleDataFromDB.populateLists();
 
-	}
+    }
 
-	@GetMapping("/")
-	public String view(HttpSession session, Model model) {
-		populate();
-		model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
-		model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
-		model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
-		model.addAttribute("customer_HashMap", handleDataFromDB.getCustomerHashMap());
-		model.addAttribute("reservation_ArrayList", handleDataFromDB.getReservationArrayList());
-		model.addAttribute("activities_HashMap", handleDataFromDB.getActivityHashMap());
-		model.addAttribute("employees_HashMap", handleDataFromDB.getEmployeeHashMap());
+    @GetMapping("/")
+    public String view(HttpSession session, Model model) {
 
-		return "/index";
-	}
+        // When going to index it should search for reservations on the date and get all from there.
 
-	@GetMapping("/reservation/create")
-	public String createStepOne(HttpSession session, Model model) {
-		//if(Authenticate.isLoggedIn(session)) {
-		model.addAttribute("activities", ActivityLogic.getActivities());
-		model.addAttribute("totalActivities", ActivityLogic.getTotalActivities() - 1);
-		return "/reservation/create_step_1";
-		//}
-		//return "redirect:/";
-	}
+        populate();
+        model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+        model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+        model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
+        model.addAttribute("customer_HashMap", handleDataFromDB.getCustomerHashMap());
+        model.addAttribute("reservation_ArrayList", handleDataFromDB.getReservationArrayList());
+        model.addAttribute("activities_HashMap", handleDataFromDB.getActivityHashMap());
+        model.addAttribute("employees_HashMap", handleDataFromDB.getEmployeeHashMap());
 
-	@GetMapping("/reservation/create/{id}")
-	public String createStepTwo(HttpSession session, Model model) {
-		//if(Authenticate.isLoggedIn(session)) {
-		model.addAttribute("activities", ActivityLogic.getActivities());
-		model.addAttribute("totalActivities", ActivityLogic.getTotalActivities() - 1);
-		return "/reservation/create_step_2";
-		//}
-		//return "redirect:/";
-	}
+        return "/index";
+    }
 
-	@PostMapping("/reservation/create")
-	public String create(HttpSession session, Reservation reservation, Customer customer, Employee employee) {
-		ReservationLogic.create(reservation, customer, employee, session);
-		return "redirect:/reservation/create";
-	}
+    // Postmapping for search option. Search should be on all activities by default.
+    // Ajax could be used when selecting activities? Find out with the other members regarding how the
+    // search function should work.
+    // Possibly only going to make a new ArrayList<Reservation> with the reservations that you conditioned for.
+    // Then use a setter (setReservationArrayList()) to set it to the new list.
+    // Then redirect back to /index.
 
-	@GetMapping("/landingpage_employer")
-	public String landingpageEmployer() {
-		return "/employer/landingpage_employer";
-	}
+    @GetMapping("/reservation/create")
+    public String createStepOne(HttpSession session, Model model) {
+        //if(Authenticate.isLoggedIn(session)) {
+        model.addAttribute("activities", ActivityLogic.getActivities());
+        model.addAttribute("totalActivities", ActivityLogic.getTotalActivities() - 1);
+        return "/reservation/create_step_1";
+        //}
+        //return "redirect:/";
+    }
+
+    @GetMapping("/reservation/create/{id}")
+    public String createStepTwo(HttpSession session, Model model) {
+        //if(Authenticate.isLoggedIn(session)) {
+        model.addAttribute("activities", ActivityLogic.getActivities());
+        model.addAttribute("totalActivities", ActivityLogic.getTotalActivities() - 1);
+        return "/reservation/create_step_2";
+        //}
+        //return "redirect:/";
+    }
+
+    @PostMapping("/reservation/create")
+    public String create(HttpSession session, Reservation reservation, Customer customer, Employee employee) {
+        ReservationLogic.create(reservation, customer, employee, session);
+        return "redirect:/reservation/create";
+    }
+
+    @GetMapping("/landingpage_employer")
+    public String landingpageEmployer() {
+        return "/employer/landingpage_employer";
+    }
 }
