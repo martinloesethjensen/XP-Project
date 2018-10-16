785cbd3f427a8e3f98876c9a6847adfe96c2bce1
MD <altair2400@gmail.com>
2018-10-16 10:39:26 +0200

    aktiviteter burde virke nu(test fase)

--- src/main/java/com/adventures2/xpproject/controller/ActivityController.java
+++ src/main/java/com/adventures2/xpproject/controller/ActivityController.java
@@ -78,7 +78,7 @@ public class ActivityController {
     succesMessage = "Aktiviten blev slettet";
     return "redirect:/aktivitetogoversigt";
     //}
-    //return "redirect:/";
+    ///return "redirect:/";
 
   }
 
