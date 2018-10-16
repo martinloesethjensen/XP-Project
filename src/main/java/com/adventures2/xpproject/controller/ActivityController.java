33d265c62467fd3f486d9116a2bcf88abb995093
capttwigg <mike@familien-jahn.dk>
2018-10-16 09:46:14 +0200

    session in activity controller

--- src/main/java/com/adventures2/xpproject/controller/ActivityController.java
+++ src/main/java/com/adventures2/xpproject/controller/ActivityController.java
@@ -1,5 +1,6 @@
 package com.adventures2.xpproject.controller;
 
+import com.adventures2.xpproject.auth.Authenticate;
 import com.adventures2.xpproject.base.Activity;
 import com.adventures2.xpproject.logic.ActivityLogic;
 
@@ -10,38 +11,75 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class ActivityController {
-    public static ArrayList<Activity> actiList = new ArrayList<>();
-    String succesMessage = "";
+  public static ArrayList<Activity> actiList = new ArrayList<>();
+  String succesMessage = "";
 
-    @GetMapping("/create_activity")
-    public String orderActivity(Model model) {
-        model.addAttribute("aktivitet", new Activity());
-        return "createActivity";
-    }
+  @GetMapping("/create_activity")
+  public String orderActivity(HttpSession session, Model model) {
+    //if(Authenticate.isLoggedIn(session)) {
+    model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+    model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+    model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
+    model.addAttribute("aktivitet", new Activity());
+    return "createActivity";
+    //}
+    //return "redirect:/";
+  }
 
-    @PostMapping("/create_activity")
-    public String orderActivity(Activity activity) {
-      ActivityLogic.createActivity(activity);
-        return "redirect:/createActivity";
-    }
+  @PostMapping("/create_activity")
+  public String orderActivity(HttpSession session, Model model, Activity activity) {
+    ActivityLogic.createActivity(activity);
+    //if(Authenticate.isLoggedIn(session)) {
+    model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+    model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+    model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
+    return "redirect:/createActivity";
+    //}
+    //return "redirect:/";
 
-    @GetMapping("edit_activity")
-  public String editActivity(){
-      return "redigAkt";
-    }
+  }
+
+  @GetMapping("edit_activity")
+  public String editActivity(HttpSession session, Model model) {
+    //if(Authenticate.isLoggedIn(session)) {
+    model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+    model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+    model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
+    return "redigAkt";
+    //}
+    //return "redirect:/";
+
+  }
+
   @PostMapping("/edit_activity")
-  public String editActivity(Activity activity) {
+  public String editActivity(HttpSession session, Model model, Activity activity) {
+    //if(Authenticate.isLoggedIn(session)) {
+    model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+    model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+    model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
     ActivityLogic.updateActivity(activity);
     return "redirect:/createActivity";
+    //}
+    //return "redirect:/";
+
   }
 
   @PostMapping("/delete_activity")
-  public String deleteActivity(Activity activity){
-      ActivityLogic.deleteActivity(activity);
-      succesMessage = "Aktiviten blev slettet";
-      return "redirect:/aktivitetogoversigt";
+  public String deleteActivity(HttpSession session, Model model, Activity activity) {
+    //if(Authenticate.isLoggedIn(session)) {
+    model.addAttribute("IS_LOGGED_IN", Authenticate.isLoggedIn(session));
+    model.addAttribute("NIVEAU", session.getAttribute("NIVEAU"));
+    model.addAttribute("REALNAME", session.getAttribute("REALNAME"));
+    ActivityLogic.deleteActivity(activity);
+    succesMessage = "Aktiviten blev slettet";
+    return "redirect:/aktivitetogoversigt";
+    //}
+    //return "redirect:/";
+
   }
 
 }
