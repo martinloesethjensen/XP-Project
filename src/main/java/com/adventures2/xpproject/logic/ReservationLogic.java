67ca917a6f2520808ec39d7257c7a31d17877e86
Martin L. Jensen <31356774+martinloesethjensen@users.noreply.github.com>
2018-10-18 22:41:20 +0200

    - Made a fix on a search problem. The problem was a search with an activity and a date that knowingly didn't have a reservation showed up. It's now fixed with crude code conditionals that might need another review.

--- src/main/java/com/adventures2/xpproject/logic/ReservationLogic.java
+++ src/main/java/com/adventures2/xpproject/logic/ReservationLogic.java
@@ -31,8 +31,8 @@ public class ReservationLogic {
 
 		try {
 			PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement(
-				"INSERT INTO reservations (start,end,customDiscount,peopleAmount,fk_customer_id,fk_activity_id,fk_user_id,fk_employee_id) " +
-					"VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
+					"INSERT INTO reservations (start,end,customDiscount,peopleAmount,fk_customer_id,fk_activity_id,fk_user_id,fk_employee_id) " +
+							"VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
 			);
 			preparedStatement.setString(1, reservation.getStart());
 			preparedStatement.setString(2, reservation.getEnd());
@@ -59,43 +59,54 @@ public class ReservationLogic {
 			long unixTimeEnd = 0;
 
 			try {
-				if (!date.equals("")) {
+				PreparedStatement preparedStatement = null;
+
+				if (!date.equals("") && activity == 0) {
 					DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 					Date tempDate = dateFormat.parse(date);
 					unixTimeStart = tempDate.getTime() / 1000;
 					unixTimeEnd = unixTimeStart + 86400;
+
+					preparedStatement = DatabaseConnection.getConnection().prepareStatement(
+							"SELECT * FROM reservations WHERE fk_activity_id = ? OR (start >= ? AND end <= ?)");
+					preparedStatement.setInt(1, activity);
+					preparedStatement.setLong(2, unixTimeStart);
+					preparedStatement.setLong(3, unixTimeEnd);
 				}
 
-				PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement(
-					"SELECT * FROM reservations WHERE fk_activity_id = ? OR (start >= ? AND end <= ?)");
-				preparedStatement.setInt(1, activity);
-				preparedStatement.setLong(2, unixTimeStart);
-				preparedStatement.setLong(3, unixTimeEnd);
-
+				if (activity > 0 && !date.equals("")) {
+					preparedStatement = DatabaseConnection.getConnection().prepareStatement(
+							"SELECT * FROM reservations WHERE fk_activity_id = ? AND (start >= ? AND end <= ?)");
+					preparedStatement.setInt(1, activity);
+					preparedStatement.setLong(2, unixTimeStart);
+					preparedStatement.setLong(3, unixTimeEnd);
+				} else if (activity > 0) {
+					preparedStatement = DatabaseConnection.getConnection().prepareStatement(
+							"SELECT * FROM reservations WHERE fk_activity_id = ? ");
+					preparedStatement.setInt(1, activity);
+				}
 				resultSet = DatabaseConnection.queryWithParameters(preparedStatement);
 			} catch (SQLException | ParseException e) {
 				e.printStackTrace();
 			}
-		}
-
-		else {
+		} else {
 			resultSet = DatabaseConnection.query("SELECT * FROM reservations");
 		}
 
 		try {
 			while (resultSet.next()) {
 				reservations.add(new Reservation(
-					resultSet.getInt("id"),
-					"Kl: " + new SimpleDateFormat("HH:mm").format(
-						new Date(Long.parseLong(resultSet.getString("start")) * 1000 - (3600 * 1000))),
-					"Kl: " + new SimpleDateFormat("HH:mm").format(
-						new Date(Long.parseLong(resultSet.getString("end")) * 1000 - (3600 * 1000))),
-					resultSet.getInt("customDiscount"),
-					resultSet.getInt("peopleAmount"),
-					resultSet.getInt("fk_customer_id"),
-					resultSet.getInt("fk_activity_id"),
-					resultSet.getInt("fk_user_id"),
-					resultSet.getInt("fk_employee_id")
+						resultSet.getInt("id"),
+						"Kl: " + new SimpleDateFormat("HH:mm").format(
+								new Date(Long.parseLong(resultSet.getString("start")) * 1000 - (3600 * 1000))),
+						"Kl: " + new SimpleDateFormat("HH:mm").format(
+								new Date(Long.parseLong(resultSet.getString("end")) * 1000 - (3600 * 1000))),
+						resultSet.getInt("customDiscount"),
+						resultSet.getInt("peopleAmount"),
+						resultSet.getInt("fk_customer_id"),
+						resultSet.getInt("fk_activity_id"),
+						resultSet.getInt("fk_user_id"),
+						resultSet.getInt("fk_employee_id")
 				));
 			}
 		} catch (SQLException e) {
@@ -105,27 +116,27 @@ public class ReservationLogic {
 		return reservations;
 	}
 
-    public static Reservation getReservationById(int id) {
-        try {
-            PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM reservations WHERE id = ? ");
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = DatabaseConnection.queryWithParameters(preparedStatement);
-            if(resultSet.next()){
-                return new Reservation(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getInt(4),
-                        resultSet.getInt(5),
-                        resultSet.getInt(6),
-                        resultSet.getInt(7),
-                        resultSet.getInt(8),
-                        resultSet.getInt(9)
-                );
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public static Reservation getReservationById(int id) {
+		try {
+			PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM reservations WHERE id = ? ");
+			preparedStatement.setInt(1, id);
+			ResultSet resultSet = DatabaseConnection.queryWithParameters(preparedStatement);
+			if (resultSet.next()) {
+				return new Reservation(
+						resultSet.getInt(1),
+						resultSet.getString(2),
+						resultSet.getString(3),
+						resultSet.getInt(4),
+						resultSet.getInt(5),
+						resultSet.getInt(6),
+						resultSet.getInt(7),
+						resultSet.getInt(8),
+						resultSet.getInt(9)
+				);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
