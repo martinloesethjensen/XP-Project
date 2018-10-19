66e3bd45dfc255ceb6016a9e5537b0514d67e598
capttwigg <mike@familien-jahn.dk>
2018-10-19 13:23:49 +0200

    defects now in folders

--- src/main/java/com/adventures2/xpproject/controller/DefectController.java
+++ src/main/java/com/adventures2/xpproject/controller/DefectController.java
@@ -2,39 +2,38 @@ package com.adventures2.xpproject.controller;
 
 import com.adventures2.xpproject.base.DefectTools;
 import com.adventures2.xpproject.logic.DefectLogic;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import static com.adventures2.xpproject.logic.DefectLogic.deletedefects;
 
 @Controller
 public class DefectController {
 
 
-@GetMapping("/Report_defects")
-public String reportDefects(Model model) {
-    model.addAttribute("defects",new DefectTools());
-    return "Report_defects";
-    }
-@PostMapping("/Report_defects")
- public String reportDefects(DefectTools defects) {
-    System.out.println(defects);
+  @GetMapping("/Report_defects")
+  public String reportDefects(Model model) {
+    model.addAttribute("defects", new DefectTools());
+    return "defects/Report_defects";
+  }
+
+  @PostMapping("/Report_defects")
+  public String reportDefects(DefectTools defects) {
     DefectLogic.writeDefectToDatabase(defects);
     return "redirect:/show_defects";
- }
-@GetMapping("/show_defects")
- public String showDefects(Model model){
-model.addAttribute("DefectArray",DefectLogic.ReadDefects());
-    return "show_defects";
-    }
+  }
 
-@GetMapping("delete_defects/{ID}")
-    public String deledefects(@PathVariable("ID") int ID,DefectLogic defects){
+  @GetMapping("/show_defects")
+  public String showDefects(Model model) {
+    model.addAttribute("DefectArray", DefectLogic.ReadDefects());
+    return "defects/show_defects";
+  }
+
+  @GetMapping("delete_defects/{ID}")
+  public String deledefects(@PathVariable("ID") int ID, DefectLogic defects) {
     DefectLogic.deletedefects(ID);
     return "redirect:/show_defects";
-    }
+  }
 }
